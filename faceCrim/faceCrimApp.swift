922ad25b9f6c6aee06422ec2911bdec8821c731a
Giray Aksakal <girayaksakal@icloud.com>
2024-12-05 00:34:55 +0300

    Initial Commit

---
+++ faceCrim/faceCrimApp.swift
@@ -0,0 +1,17 @@
+//
+//  faceCrimApp.swift
+//  faceCrim
+//
+//  Created by Giray Aksakal on 5.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct faceCrimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
